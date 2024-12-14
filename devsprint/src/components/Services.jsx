8d981e00d7d3727790ed3bcc032a538e070ef433
boulanouarmohamed1 <nm_boulanouar@esi.dz>
2024-12-14 14:19:16 +0100

    random push

--- devsprint/src/components/Services.jsx
+++ devsprint/src/components/Services.jsx
@@ -9,23 +9,34 @@ const Services = () => {
           title: "UI/UX Design",
           image: "/assets/Frame 61.png",
         },
-        {
-          title: "UI/UX Design",
-          image: "/assets/Frame 61.png",
-        },
-        {
-          title: "UI/UX Design",
-          image: "/assets/Frame 61.png",
-        },
+        
       ];
   return (
-    <div className="bg-white pt-6">
-      <div className="flex overflow-x-auto space-x-4 pb-4 sm:space-x-6 lg:space-x-8">
+    <div className="bg-white m-14  flex flex-col text-left justify-center items-center gap-28 p-10">
+      <div className="flex justify-between items-center gap-10 w-full">
+        <div className="text-left ">
+          <h1 className="text-5xl font-bold mb-5">Let's have a look at</h1>
+          <h1 className="text-5xl font-bold">my <span className="text-blue-500">Portfolio</span></h1>
+        </div>
+        <button className="px-6 text-white py-3 text-xl rounded-3xl font-bold bg-blue-500">See All</button>
+      </div>
+      <div className="flex justify-between w-full  ">
         {services.map((service, index) => (
-          <div key={index} className="flex-none w-full sm:w-1/2 lg:w-1/3">
+          <div key={index} className="">
             <Service titre={service.title} image={service.image} />
           </div>
         ))}
+      </div>
+      <div className="flex justify-between items-center gap-5 ">
+        <button className="px-5 py-2 rounded-3xl bg-slate-300">Landing Page</button>
+        <button className="px-5 py-2 rounded-3xl bg-slate-300">Landing Page</button>
+        <button className="px-5 py-2 rounded-3xl bg-slate-300">Landing Page</button>
+        <button className="px-5 py-2 rounded-3xl bg-slate-300">Landing Page</button>
+        <button className="px-5 py-2 rounded-3xl bg-slate-300">Landing Page</button>
+      </div>
+      <div className="flex justify-between items-center gap-5 flex-col ">
+      <h1 className="text-5xl font-bold mb-5">Lirante - Food Dilvery Solution</h1>
+      <h1 className="text-xl  mb-5 w-1/2">LLorem ipsum dolor sit amet, consectetur adipiscing elit. Sed congue interdum ligula a dignissim. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lobortis orci elementum egestas lobortis.</h1>
       </div>
     </div>
   )
