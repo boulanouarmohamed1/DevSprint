51a177a691bbd4f40826de92e10d20278e18c702
boulanouarmohamed1 <nm_boulanouar@esi.dz>
2024-12-14 14:46:07 +0100

    mzal chwiya

--- devsprint/src/pages/index.jsx
+++ devsprint/src/pages/index.jsx
@@ -1,8 +1,21 @@
+import Discuss from "@/components/Discuss/Discuss";
+import HireMe from "@/components/HireMe/HireMe";
+import MyResult from "@/components/MYResulta";
+import Mywork from "@/components/Mywork";
+import Footer from "@/componets/Footer/Footer";
 import NavBar from "@/compument/Navbar/Navbar";
 
 export default function Home() {
   return (
-   
+    <>
     <NavBar></NavBar>
+    <Mywork></Mywork>
+    <HireMe></HireMe>
+    <MyResult></MyResult>
+    <Discuss></Discuss>
+    <Footer></Footer>
+   </>
+   
+
   );
 }
